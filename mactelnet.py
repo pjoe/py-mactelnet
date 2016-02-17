6662fd086ab305dd835ae782194853b5ad662225
Pelle Johnsen <pelle.johnsen@gmail.com>
2016-02-17 20:48:59 +0100

    Refactor and handle commands
    Separate out Connection class

--- mactelnet.py
+++ mactelnet.py
@@ -6,6 +6,7 @@ import binascii
 import StringIO
 import hashlib
 import random
+import re
 from socket import *
 from struct import *
 from select import select
@@ -81,7 +82,7 @@ class Message(object):
 
         if not self.plain_data is None:
             buf.write(self.plain_data)
-        return buf
+        return buf.getvalue()
 
     def parse(self, data):
         self.version, self.type, self.src_mac_raw, self.dst_mac_raw, self.client_type, \
@@ -126,62 +127,99 @@ def parse_msg(msg, session_key):
         return None
     return parsed
 
-def send_msg(sock, msg):
-    pass
-
 def parse_mac(mac_str):
     return binascii.unhexlify(mac_str.replace(':',''))
 
 def mac_to_str(mac):
     return "%02x:%02x:%02x:%02x:%02x:%02x" % tuple(ord(x) for x in mac)
 
-def find_interface(dst_mac, insock, inport, session_key):
-    for iface in netifaces.interfaces():
-        if iface.startswith('lo'):
-            continue
-        addrs = netifaces.ifaddresses(iface)
-        if not addrs.has_key(netifaces.AF_INET):
-            continue
-        src_mac = addrs[netifaces.AF_LINK][0]['addr']
-        src_ip = addrs[netifaces.AF_INET][0]['addr']
-        if not src_mac or not src_ip:
-            continue
-        print "Trying if:", iface, src_ip, src_mac
-        testsock = socket(AF_INET, SOCK_DGRAM)
-        testsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        testsock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-        testsock.bind((src_ip, inport))
+class Connection(object):
+    def __init__(self, dst_mac):
+        self.incounter = -1
+        self.outcounter = 0
+        self.session_key = random.randint(0, 0xffff)
+        self.insock = socket(AF_INET, SOCK_DGRAM)
+        self.insock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.insock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        self.insock.bind(('', 0))
+        _, self.inport = self.insock.getsockname()
+        #print "Local port:", self.inport
 
-        msg = Message(MT_PTYPE_SESSIONSTART, src_mac, dst_mac, session_key, 0)
-        msg_data = msg.get_buffer().getvalue()
-        testsock.sendto(msg_data, ('255.255.255.255', MACTELNET_PORT))
+        self.dst_mac = dst_mac
+        if not self.find_interface():
+            print "Error: can't find interface to use for connection"
+            sys.exit(1)
 
-        inlist, _, _ = select((insock,), (), (), 2)
-        if len(inlist) > 0:
-            return (testsock, src_mac, src_ip)
-        testsock.close()
 
-    return (None, None)
+    def find_interface(self):
+        for iface in netifaces.interfaces():
+            if iface.startswith('lo'):
+                continue
+            addrs = netifaces.ifaddresses(iface)
+            if not addrs.has_key(netifaces.AF_INET):
+                continue
+            src_mac = addrs[netifaces.AF_LINK][0]['addr']
+            src_ip = addrs[netifaces.AF_INET][0]['addr']
+            if not src_mac or not src_ip:
+                continue
+            print "Trying if:", iface, src_ip, src_mac
+            testsock = socket(AF_INET, SOCK_DGRAM)
+            testsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            testsock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+            testsock.bind((src_ip, self.inport))
 
-def get_reply(insock, outsock, session_key):
-    data = insock.recv(1500)
-    #print "Recv:", len(data), binascii.hexlify(data)
-    reply = parse_msg(data, session_key)
-    if reply:
-        if reply.type == MT_PTYPE_DATA:
-            # send ack
-            msg = Message(MT_PTYPE_ACK, reply.dst_mac, reply.src_mac, session_key, reply.counter + len(data) - MT_HEADER_LEN)
-            msg_data = msg.get_buffer().getvalue()
-            outsock.sendto(msg_data, ('255.255.255.255', MACTELNET_PORT))
-    return reply
+            msg = Message(MT_PTYPE_SESSIONSTART, src_mac, self.dst_mac, self.session_key, 0)
+            testsock.sendto(msg.get_buffer(), ('255.255.255.255', MACTELNET_PORT))
 
-def send_msg(msg, insock, outsock, session_key, need_ack=True):
-    #print "Sending:", msg
-    msg_data = msg.get_buffer().getvalue()
-    outsock.sendto(msg_data, ('255.255.255.255', MACTELNET_PORT))
-    if need_ack:
-        ack = get_reply(insock, outsock, session_key)
-    return len(msg_data) - MT_HEADER_LEN
+            inlist, _, _ = select((self.insock,), (), (), 2)
+            if len(inlist) > 0:
+                self.outsock = testsock
+                self.src_mac = src_mac
+                self.src_ip = src_ip
+                return True
+            testsock.close()
+
+        return False
+
+    def get_reply(self):
+        while True:
+            data = self.insock.recv(1500)
+            #print "Recv:", len(data), binascii.hexlify(data)
+            reply = parse_msg(data, self.session_key)
+            #print " parsed:", reply
+            if reply:
+                if reply.type == MT_PTYPE_DATA:
+                    # send ack
+                    msg = Message(MT_PTYPE_ACK, reply.dst_mac, reply.src_mac, self.session_key, reply.counter + len(data) - MT_HEADER_LEN)
+                    msg_data = msg.get_buffer()
+                    self.outsock.sendto(msg_data, ('255.255.255.255', MACTELNET_PORT))
+                    if reply.counter > self.incounter or self.incounter - reply.counter > 65535:
+                        self.incounter = reply.counter
+                    else:
+                        continue
+            return reply
+
+    def send_msg(self, msg, need_ack=True):
+        #print "Sending:", msg
+        msg_data = msg.get_buffer()
+        self.outsock.sendto(msg_data, ('255.255.255.255', MACTELNET_PORT))
+        if need_ack:
+            ack = self.get_reply()
+        self.outcounter += len(msg_data) - MT_HEADER_LEN
+
+    def new_message(self, type):
+        return Message(type, self.src_mac, self.dst_mac, self.session_key, self.outcounter)
+
+def clean_esc(s):
+    s = re.sub('\x1b\\[\\d+[ABCD]', '', s)
+    s = re.sub('\x1b[DZ]', '', s)
+    s = re.sub('\x1b\\[6n', '', s)
+    s = re.sub('\x1b\\[H', '', s)
+    s = re.sub('\x1b\\[\\d*m', '', s)
+    s = re.sub('\x1b\\[[?0-9]*[lh]', '', s)
+    s = re.sub('\x1b\\[[;0-9]*r', '', s)
+    s = re.sub('\x1b', '<ESC>', s)
+    return s
 
 def main():
     if len(sys.argv) < 2:
@@ -195,27 +233,19 @@ def main():
     user = "admin"
     passwd = ""
 
-    session_key = random.randint(0, 0xffff)
-    insock = socket(AF_INET, SOCK_DGRAM)
-    insock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    insock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    insock.bind(('', 0))
-    _, inport = insock.getsockname()
-    print "Local port:", inport
 
-    outsock, src_mac, src_ip = find_interface(dst_mac, insock, inport, session_key)
-    print "Using:", src_mac, src_ip
+    conn = Connection(dst_mac)
 
     # ACK for session start
-    reply = get_reply(insock, outsock, session_key)
-    #print "ACK:", reply
+    reply = conn.get_reply()
+    # print "ACK:", reply
 
-    counter = 0
-    msg = Message(MT_PTYPE_DATA, src_mac, dst_mac, session_key, counter)
+    # authentication
+    msg = conn.new_message(MT_PTYPE_DATA)
     msg.add_control(MT_CPTYPE_BEGINAUTH, None)
-    counter += send_msg(msg, insock, outsock, session_key)
+    conn.send_msg(msg)
 
-    reply = get_reply(insock, outsock, session_key)
+    reply = conn.get_reply()
     #print "Reply:", reply
     enc_key = ''
     if reply.type == MT_PTYPE_DATA:
@@ -223,15 +253,15 @@ def main():
             if cptype == MT_CPTYPE_ENCRYPTIONKEY:
                 enc_key = cpdata
 
-    msg = Message(MT_PTYPE_DATA, src_mac, dst_mac, session_key, counter)
+    msg = conn.new_message(MT_PTYPE_DATA)
     passwd_hash = '\x00' + hashlib.md5('\x00' + passwd + enc_key).digest()
     msg.add_control(MT_CPTYPE_PASSWORD, passwd_hash)
     msg.add_control(MT_CPTYPE_USERNAME, user)
     msg.add_control(MT_CPTYPE_TERM_TYPE, 'ansi')
     msg.add_control(MT_CPTYPE_TERM_WIDTH, pack('!H', 80))
     msg.add_control(MT_CPTYPE_TERM_HEIGHT, pack('!H', 25))
-    counter += send_msg(msg, insock, outsock, session_key)
-    reply = get_reply(insock, outsock, session_key)
+    conn.send_msg(msg)
+    reply = conn.get_reply()
     #print "Reply:", reply
 
     commands = [
@@ -239,20 +269,22 @@ def main():
     ]
 
     while True:
-        reply = get_reply(insock, outsock, session_key)
+        reply = conn.get_reply()
         #print "\nReply:", reply
         if reply.plain_data:
-            sys.stdout.write('\n<<\n' + reply.plain_data.replace('\x1b', '^[') + '\n>>\n')
+            print "\n" + binascii.hexlify(reply.plain_data)
+            sys.stdout.write('<<\n' + reply.plain_data.replace('\x1b', '<ESC>') + '\n>>\n\n')
+            sys.stdout.write(clean_esc(reply.plain_data))
             sys.stdout.flush()
-            if reply.plain_data.endswith('\x1b[6n'):
+            if reply.plain_data.count('\x1b[6n') > 0:
                 # asking for cursor pos, give some reply
-                msg = Message(MT_PTYPE_DATA, src_mac, dst_mac, session_key, counter)
-                msg.add_plain('\x1b[1;80R')
-                counter += send_msg(msg, insock, outsock, session_key)
+                msg = conn.new_message(MT_PTYPE_DATA)
+                msg.add_plain('\x1b[1;80R' * reply.plain_data.count('\x1b[6n'))
+                conn.send_msg(msg)
             if reply.plain_data.endswith('> ') and len(commands):
-                msg = Message(MT_PTYPE_DATA, src_mac, dst_mac, session_key, counter)
+                msg = conn.new_message(MT_PTYPE_DATA)
                 msg.add_plain(commands.pop(0))
-                counter += send_msg(msg, insock, outsock, session_key)
+                conn.send_msg(msg)
 
         if reply.type == MT_PTYPE_END:
             break
